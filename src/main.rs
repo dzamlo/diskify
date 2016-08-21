9e35e802aefaf549c2923f6c14ea4b07eeac7319
Loïc Damien <loic.damien@dzamlo.ch>
2016-08-21 23:56:14 +0200

    Add cli options

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
+extern crate clap;
 extern crate image;
 
 use std::path::Path;
 
+use clap::{Arg, App};
 use image::{ImageBuffer, Rgb};
 
 mod disk;
 use disk::Disk;
 
 const BLACK: Rgb<u8> = Rgb::<u8> { data: [0, 0, 0] };
-const MIN_WIDTH: u32 = 8;
+
+fn validate_min_width(val: String) -> Result<(), String> {
+    if val.parse::<u32>().is_err() {
+        Err(String::from("the miminum width must an integer in the range 0-4294967295"))
+    } else {
+        Ok(())
+    }
+}
+
+fn validate_iterations(val: String) -> Result<(), String> {
+    if val.parse::<u32>().is_err() {
+        Err(String::from("the number of iterations must an integer in the range 0-4294967295"))
+    } else {
+        Ok(())
+    }
+}
 
 pub fn choose_background<'a, D>(disks: D, img: &ImageBuffer<Rgb<u8>, Vec<u8>>) -> Rgb<u8>
     where D: IntoIterator<Item = &'a Disk>
@@ -32,7 +49,36 @@ pub fn choose_background<'a, D>(disks: D, img: &ImageBuffer<Rgb<u8>, Vec<u8>>) -
 }
 
 fn main() {
-    let img = image::open(&Path::new("test.jpg")).unwrap().to_rgb();
+    let app = App::new("diskify")
+        .version(env!("CARGO_PKG_VERSION"))
+        .arg(Arg::with_name("min_width")
+            .short("m")
+            .long("min-width")
+            .help("minimum width of the disks")
+            .takes_value(true)
+            .validator(validate_min_width)
+            .default_value("8"))
+        .arg(Arg::with_name("iterations")
+            .short("i")
+            .long("iterations")
+            .help("the number of iterations")
+            .takes_value(true)
+            .validator(validate_iterations)
+            .default_value("1024"))
+        .arg(Arg::with_name("input")
+            .help("input image")
+            .required(true))
+        .arg(Arg::with_name("output")
+            .help("where to save the result")
+            .required(true));
+    let matches = app.get_matches();
+    let input = matches.value_of("input").unwrap();
+    let output = matches.value_of("output").unwrap();
+    let min_width = matches.value_of("min_width").unwrap().parse().unwrap();
+    let iterations = matches.value_of("iterations").unwrap().parse().unwrap();
+
+
+    let img = image::open(&Path::new(input)).unwrap().to_rgb();
     assert!(img.width() == img.height(), "The image must be a square");
     let mut initial_disk = Disk::new(0, 0, img.width(), BLACK);
     let color = initial_disk.choose_color(&img);
@@ -40,11 +86,11 @@ fn main() {
     let initial_disk_cost = initial_disk.cost(&img);
     let mut disks = vec![(initial_disk_cost, initial_disk)];
 
-    for i in 0..4096 {
-        println!("{}", i);
+    for i in 0..iterations {
+        println!("iteration {}/{}", i + 1, iterations);
         disks.sort_by(|&(cost1, _), &(cost2, _)| cost1.partial_cmp(&cost2).unwrap());
         let (_, d) = disks.pop().unwrap();
-        if d.width() <= MIN_WIDTH {
+        if d.width() <= min_width {
             disks.push((0.0, d));
             break;
         }
@@ -52,7 +98,7 @@ fn main() {
         for disk in &mut split {
             let color = disk.choose_color(&img);
             disk.set_color(color);
-            let cost = if disk.width() <= MIN_WIDTH {
+            let cost = if disk.width() <= min_width {
                 0.0
             } else {
                 disk.cost(&img)
@@ -67,5 +113,5 @@ fn main() {
         d.draw(&mut img);
     }
 
-    img.save("test.png").unwrap();
+    img.save(output).unwrap();
 }
