39965404fd91b52f5e7d8a65fb12d77091f62caf
Loïc Damien <loic.damien@dzamlo.ch>
2016-08-21 21:11:00 +0200

    First implementation

---
+++ src/disk.rs
@@ -0,0 +1,117 @@
+use image::{GenericImage, Rgb};
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Disk {
+    top: u32,
+    left: u32,
+    width: u32,
+    color: Rgb<u8>,
+}
+
+impl Disk {
+    pub fn new(top: u32, left: u32, width: u32, color: Rgb<u8>) -> Disk {
+        Disk {
+            top: top,
+            left: left,
+            width: width,
+            color: color,
+        }
+    }
+
+    pub fn inside(&self, x: u32, y: u32) -> bool {
+        let center_x = self.left + self.width / 2;
+        let center_y = self.top + self.width / 2;
+        let center_square = (self.width / 2) * (self.width / 2);
+        square_abs_diff(x, center_x) + square_abs_diff(y, center_y) <= center_square
+    }
+
+    pub fn set_color(&mut self, color: Rgb<u8>) {
+        self.color = color;
+    }
+
+    pub fn choose_color<I>(&self, img: &I) -> Rgb<u8>
+        where I: GenericImage<Pixel = Rgb<u8>>
+    {
+        let mut sums = [0u32; 3];
+        let mut count = 0;
+        for x in self.left..(self.left + self.width) {
+            for y in self.top..(self.top + self.width) {
+                let pixel = img.get_pixel(x, y);
+                if self.inside(x, y) {
+                    count += 1;
+                    for i in 0..sums.len() {
+                        sums[i] += pixel[i] as u32;
+                    }
+                }
+            }
+        }
+
+        for channel in &mut sums {
+            if count > 0 {
+                *channel /= count;
+            }
+        }
+
+        Rgb { data: [sums[0] as u8, sums[1] as u8, sums[2] as u8] }
+    }
+
+    pub fn cost<I>(&self, img: &I) -> f64
+        where I: GenericImage<Pixel = Rgb<u8>>
+    {
+        let mut sum = 0u64;
+        let mut count = 0u64;
+
+        for x in self.left..(self.left + self.width) {
+            for y in self.top..(self.top + self.width) {
+                let pixel = img.get_pixel(x, y);
+                if self.inside(x, y) {
+                    count += pixel.data.len() as u64;
+                    for (channel_pixel, channel_self) in pixel.data
+                        .into_iter()
+                        .zip(&self.color.data) {
+                        sum += square_abs_diff(*channel_pixel as u32, *channel_self as u32) as u64
+                    }
+                }
+            }
+        }
+
+        sum as f64 / count as f64
+    }
+
+    pub fn split(&self) -> [Disk; 4] {
+        let split_at = self.width / 2;
+        [Disk::new(self.top, self.left, split_at, self.color),
+         Disk::new(self.top + split_at, self.left, split_at, self.color),
+         Disk::new(self.top, self.left + split_at, split_at, self.color),
+         Disk::new(self.top + split_at,
+                   self.left + split_at,
+                   split_at,
+                   self.color)]
+    }
+
+    pub fn draw<I>(&self, img: &mut I)
+        where I: GenericImage<Pixel = Rgb<u8>> + 'static
+    {
+        for x in self.left..(self.left + self.width) {
+            for y in self.top..(self.top + self.width) {
+                if self.inside(x, y) {
+                    img.put_pixel(x, y, self.color);
+                }
+            }
+        }
+
+    }
+}
+
+fn abs_diff(a: u32, b: u32) -> u32 {
+    if a < b {
+        (b - a)
+    } else {
+        (a - b)
+    }
+}
+
+fn square_abs_diff(a: u32, b: u32) -> u32 {
+    let diff = abs_diff(a, b);
+    diff * diff
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+extern crate image;
+
+use std::path::Path;
+
+use image::Rgb;
+
+mod disk;
+use disk::Disk;
+
+const BLACK: Rgb<u8> = Rgb::<u8> { data: [0, 0, 0] };
+
 fn main() {
-    println!("Hello, world!");
+    let img = image::open(&Path::new("test.jpg")).unwrap().to_rgb();
+    assert!(img.width() == img.height(), "The image must be a square");
+    let mut initial_disk = Disk::new(0, 0, img.width(), BLACK);
+    let color = initial_disk.choose_color(&img);
+    initial_disk.set_color(color);
+    let initial_disk_cost = initial_disk.cost(&img);
+    let mut disks = vec![(initial_disk_cost, initial_disk)];
+
+    for i in 0..4096 {
+        println!("{}", i);
+        disks.sort_by(|&(cost1, _), &(cost2, _)| cost1.partial_cmp(&cost2).unwrap());
+        let (_, d) = disks.pop().unwrap();
+        let mut split = d.split();
+        for disk in &mut split {
+            let color = disk.choose_color(&img);
+            disk.set_color(color);
+            let cost = disk.cost(&img);
+            disks.push((cost, disk.clone()));
+        }
+    }
+
+    let mut img = image::ImageBuffer::new(img.width(), img.height());
+    for (_, d) in disks {
+        d.draw(&mut img);
+    }
+
+    img.save("test.png").unwrap();
 }
